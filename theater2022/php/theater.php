03d1c797a11b6d2360c08ce2330622ac30e33284
BB xie <91023055+xielol0629@users.noreply.github.com>
2022-06-21 19:53:56 +0800

    version1
    IMovie-找影城網頁

---
+++ theater2022/php/theater.php
@@ -0,0 +1,411 @@
+<?php  
+
+   $theater = $_POST["value"];
+
+   echo "影城查詢 <BR><BR>";
+
+   echo "地址：";
+
+   switch ($theater) {
+        case 1:
+            echo "台北市長春路176號 <BR>";
+            break;
+        case 2:
+            echo "台北市中山區林森北路247號4樓 <BR>";
+            break;
+        case 3:
+            echo "台北市中山區中山北路2段18號 <BR>";
+            break;
+        case 4:
+            echo "台北市中山區敬業三路22號6樓 <BR>";
+            break;
+        case 5:
+            echo "台北市萬華區西寧南路36號4樓(西門町獅子林商業大樓) <BR>";
+            break;
+        case 6:
+            echo "台北市萬華區武昌街二段89號 <BR>";
+            break;
+        case 7:
+            echo "台北市萬華區成都路88號 <BR>";
+            break;
+        case 8:
+            echo "台北市萬華區武昌街二段85號 <BR>";
+            break;
+        case 9:
+            echo "台北市萬華區漢中街116號7樓 <BR>";
+            break;
+        case 10:
+            echo "台北市萬華區漢中街52號10、11樓 <BR>";
+            break;
+        case 11:
+            echo "台北市信義區松壽路18號 <BR>";
+            break;
+        case 12:
+            echo "台北市松山區長安東路二段219號3樓 <BR>";
+            break;
+        case 13:
+            echo "台北市內湖區康寧路三段72號6樓 <BR>";
+            break;
+        case 14:
+            echo "台北市文山區羅斯福路四段200號4樓 <BR>";
+            break;
+        case 15:
+            echo "台北市中正區羅斯福路四段136巷3號 <BR>";
+            break;
+        case 16:
+            echo "台北市西門町峨眉街52號4樓 <BR>";
+            break;
+        case 17:
+            echo "台北市大同區市民大道一段209號5樓 <BR>";
+            break;
+        case 18:
+            echo "台北市和平東路3段63號2樓 <BR>";
+            break;
+        case 19:
+            echo "台北市中山區林森北路247號3樓 <BR>";
+            break;
+        case 20:
+            echo "台北市松山區民生東路五段190號3樓 <BR>";
+            break;
+        case 21:
+            echo "臺北市100中正區八德路一段1號（華山文創園區，中六館） <BR>";
+            break;
+        case 22:
+            echo "台北市信義區菸廠路80號B2 <BR>";
+            break;
+        case 23:
+            echo "台北市南港區忠孝東路7段299號11、12、13、14樓 <BR>";
+            break;
+        case 24:
+            echo "台北市中山區北安路780號B2 <BR>";
+            break;
+        case 25:
+            echo "台北市信義區松仁路58號10樓 <BR>";
+            break;
+        case 26:
+            echo "台北市士林區忠誠路二段202號 <BR>";
+            break;
+        case 27:
+            echo "台北市文山區羅斯福路6段403號4樓 <BR>";
+            break;
+        case 28:
+            echo "台北市大安區通化街24巷1號 <BR>";
+            break;
+        case 29:
+            echo "新北市板橋區館前西路158號1樓 <BR>";
+            break;
+        case 30:
+            echo "新北市新莊區民安路188巷5號 <BR>";
+            break;
+        case 31:
+            echo "新北市三重區重新路二段78號4F(天台廣場) <BR>";
+            break;
+        case 32:
+            echo "新北市板橋區新站路28號10樓 <BR>";
+            break;
+        case 33:
+            echo "新北市板橋區縣民大道2段3號 <BR>";
+            break;
+        case 34:
+            echo "新北市林口區文化三路一段402巷2號4F（昕境廣場） <BR>";
+            break;
+        case 35:
+            echo "新北市新莊區五工路66號3F <BR>";
+            break;
+        case 36:
+            echo "新北市林口區文化三路一段356號3樓 <BR>";
+            break;
+        case 37:
+            echo "新北市林口區文化三路一段356號3樓 <BR>";
+            break;
+        case 38:
+            echo "238新北市樹林區樹新路40-6號 <BR>";
+            break;
+        case 39:
+            echo "238新北市樹林區樹新路40-6號 <BR>";
+            break;
+        case 40:
+            echo "新北市土城區學府路二段210號1樓 <BR>";
+            break;
+        case 41:
+            echo "新北市淡水區義山路2段303號2樓 <BR>";
+            break;
+        case 42:
+            echo "新北市永和區中山路一段238號4F <BR>";
+            break;
+        case 43:
+            echo "新北市淡水區中正路一段2號(禮萊廣場) <BR>";
+            break;
+        case 44:
+            echo "242新北市新莊區新北大道4段3號8樓 <BR>";
+            break;
+        case 45:
+            echo "新北市新莊區新北大道4段3號10樓 <BR>";
+            break;
+        case 46:
+            echo "新北市中和區中山路三段122號4樓 <BR>";
+            break;
+        case 47:
+            echo "桃園市中壢區中園路二段509號6樓 <BR>";
+            break;
+        case 48:
+            echo "桃園市中壢區九和一街48號3樓 <BR>";
+            break;
+        case 49:
+            echo "桃園市中正路56號3-5樓 <BR>";
+            break;
+        case 50:
+            echo "桃園市蘆竹區南崁路一段112號7樓 <BR>";
+            break;
+        case 51:
+            echo "桃園市蘆竹區南崁路一段112號7樓(台茂購物中心7樓) <BR>";
+            break;
+        case 52:
+            echo "桃園市八德區介壽路一段728號3樓 <BR>";
+            break;
+        case 53:
+            echo "桃園市桃園區中正路61號9樓 <BR>";
+            break;
+        case 54:
+            echo "桃園市中壢區春德路107號 <BR>";
+            break;
+        case 55:
+            echo "桃園市中壢區高鐵南路二段352號4樓 <BR>";
+            break;
+        case 56:
+            echo "新竹市東區西大路323號8樓 <BR>";
+            break;
+        case 57:
+            echo "新竹市文昌街39號 <BR>";
+            break;
+        case 58:
+            echo "新竹市北門街6號1樓 <BR>";
+            break;
+        case 59:
+            echo "新竹市東區林森路32號7樓 <BR>";
+            break;
+        case 60:
+            echo "新竹市東區民權路176號4樓之3 <BR>";
+            break;
+        case 61:
+            echo "新竹市西大路323號8樓 <BR>";
+            break;
+        case 62:
+            echo "新竹市東區中正路65號 <BR>";
+            break;
+        case 63:
+            echo "叁 <BR>";
+            break;
+        case 64:
+            echo "肆 <BR>";
+            break;
+        case 65:
+            echo "伍 <BR>";
+            break;
+        case 66:
+            echo "陸 <BR>";
+            break;
+        case 67:
+            echo "柒 <BR>";
+            break;
+        case 68:
+            echo "捌 <BR>";
+            break;
+        case 69:
+            echo "玖 <BR>";
+            break;
+        case 70:
+            echo "玖 <BR>";
+            break;
+        case 71:
+            echo "壹 <BR>";
+            break;
+        case 72:
+            echo "貳 <BR>";
+            break;
+        case 73:
+            echo "叁 <BR>";
+            break;
+        case 74:
+            echo "肆 <BR>";
+            break;
+        case 75:
+            echo "伍 <BR>";
+            break;
+        case 76:
+            echo "陸 <BR>";
+            break;
+        case 77:
+            echo "柒 <BR>";
+            break;
+        case 78:
+            echo "捌 <BR>";
+            break;
+        case 79:
+            echo "玖 <BR>";
+            break;
+        case 80:
+            echo "玖 <BR>";
+            break;
+        case 81:
+            echo "柒 <BR>";
+            break;
+        case 82:
+            echo "捌 <BR>";
+            break;
+        case 83:
+            echo "玖 <BR>";
+            break;
+        case 84:
+            echo "玖 <BR>";
+            break;
+        case 85:
+            echo "壹 <BR>";
+            break;
+        case 86:
+            echo "貳 <BR>";
+            break;
+        case 87:
+            echo "叁 <BR>";
+            break;
+        case 88:
+            echo "肆 <BR>";
+            break;
+        case 89:
+            echo "伍 <BR>";
+            break;
+        case 90:
+            echo "陸 <BR>";
+            break;
+        case 91:
+            echo "柒 <BR>";
+            break;
+        case 92:
+            echo "捌 <BR>";
+            break;
+        case 93:
+            echo "玖 <BR>";
+            break;
+        case 94:
+            echo "玖 <BR>";
+            break;
+        case 95:
+            echo "壹 <BR>";
+            break;
+        case 96:
+            echo "貳 <BR>";
+            break;
+        case 97:
+            echo "叁 <BR>";
+            break;
+        case 98:
+            echo "肆 <BR>";
+            break;
+        case 99:
+            echo "伍 <BR>";
+            break;
+        case 100:
+            echo "陸 <BR>";
+            break;
+        case 101:
+            echo "柒 <BR>";
+            break;
+        case 102:
+            echo "捌 <BR>";
+            break;
+        case 103:
+            echo "玖 <BR>";
+            break;
+        case 104:
+            echo "玖 <BR>";
+            break;
+        case 105:
+            echo "壹 <BR>";
+            break;
+        case 106:
+            echo "貳 <BR>";
+            break;
+        case 107:
+            echo "叁 <BR>";
+            break;
+        case 108:
+            echo "肆 <BR>";
+            break;
+        case 109:
+            echo "伍 <BR>";
+            break;
+        case 110:
+            echo "陸 <BR>";
+            break;
+        case 111:
+            echo "柒 <BR>";
+            break;
+        case 112:
+            echo "捌 <BR>";
+            break;
+        case 113:
+            echo "玖 <BR>";
+            break;
+        case 114:
+            echo "玖 <BR>";
+            break;
+        case 115:
+            echo "壹 <BR>";
+            break;
+        case 116:
+            echo "貳 <BR>";
+            break;
+        case 117:
+            echo "叁 <BR>";
+            break;
+        case 118:
+            echo "肆 <BR>";
+            break;
+        case 119:
+            echo "伍 <BR>";
+            break;
+        case 120:
+            echo "陸 <BR>";
+            break;
+        case 121:
+            echo "柒 <BR>";
+            break;
+        case 122:
+            echo "捌 <BR>";
+            break;
+        case 123:
+            echo "玖 <BR>";
+            break;
+        case 124:
+            echo "玖 <BR>";
+            break;
+        case 125:
+            echo "壹 <BR>";
+            break;
+        case 126:
+            echo "貳 <BR>";
+            break;
+        case 127:
+            echo "叁 <BR>";
+            break;
+        case 128:
+            echo "肆 <BR>";
+            break;
+        case 129:
+            echo "伍 <BR>";
+            break;
+        case 130:
+            echo "陸 <BR>";
+            break;
+        case 131:
+            echo "柒 <BR>";
+            break;
+        case 132:
+            echo "捌 <BR>";
+            break;
+        case 133:
+            echo "玖 <BR>";
+            break;
+                        
+    }
+?>
